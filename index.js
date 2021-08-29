39168a2039b6660dff43003b67ac8bb955650409
DanPlayz0 <damuffin36@gmail.com>
2021-08-29 01:05:31 -0700

    first commit

---
+++ index.js
@@ -0,0 +1,16 @@
+class DiscordMoment {
+  constructor() {
+    this.version = require('./package.json').version;
+  }
+
+  format(date = Date.now(), type = 'f') {
+    if(new Date(date).getTime() === 'Invalid Date') throw Error(`Invalid Date: ${date}`);
+    if(typeof type !== 'string') throw TypeError('type must be a string');
+    if(!/^[tTdDfFR]$/.test(type)) throw new TypeError(`Invalid style: ${type}`);
+    return `<t:${Math.round(new Date(date).getTime()/1000)}:${type}>`;
+  }
+}
+
+const instance = new DiscordMoment();
+for (const key of Object.getOwnPropertyNames(Object.getPrototypeOf(instance)).filter(k => k !== 'constructor')) DiscordMoment[key] = instance[key];
+module.exports = DiscordMoment;
